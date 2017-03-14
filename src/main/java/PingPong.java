039c0ee55c493c32685d7541a47f0e07910c9b59
Alex Lee <doomcat9167@gmail.com>
2017-03-14 16:19:59 -0700

    add simple add function for jUnit testing

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PingPong {
+
+  public static int add(int x, int y){
+      return x + y;
+  }
+
+}
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -1,25 +1,13 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PingPongTest {
 
   @Test
   public void runPingPong_countUpToOne_ArrayList() {
-    PingPong testPingPong = new PingPong();
-    List<Object> expectedOutput = new ArrayList<Object>();
-    expectedOutput.add(1);
-    assertEquals(expectedOutput, testPingPong.runPingPong(1));
+    assertEquals(10, PingPong.add(6,4));
   }
-
-  @Test
-  public void runPingPong_countUpToGivenNumber_ArrayList() {
-   PingPong testPingPong = new PingPong();
-   List<Object> expectedOutput = new ArrayList<Object>();
-   expectedOutput.add(1);
-   expectedOutput.add(2);
-   assertEquals(expectedOutput, testPingPong.runPingPong(2));
- }
 
 }
