3b1e1243788c0085a33d84fce223be96b728225e
Alex Lee <doomcat9167@gmail.com>
2017-03-14 15:14:37 -0700

    initialize gradle structure

---
+++ src/test/java/PingPongTest.java
@@ -0,0 +1,25 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class PingPongTest {
+
+  @Test
+  public void runPingPong_countUpToOne_ArrayList() {
+    PingPong testPingPong = new PingPong();
+    List<Object> expectedOutput = new ArrayList<Object>();
+    expectedOutput.add(1);
+    assertEquals(expectedOutput, testPingPong.runPingPong(1));
+  }
+
+  @Test
+  public void runPingPong_countUpToGivenNumber_ArrayList() {
+   PingPong testPingPong = new PingPong();
+   List<Object> expectedOutput = new ArrayList<Object>();
+   expectedOutput.add(1);
+   expectedOutput.add(2);
+   assertEquals(expectedOutput, testPingPong.runPingPong(2));
+ }
+
+}
